7d3bcb0ece039b93112c05a93a6cb89807df22ae
Janne Kärki <janne.karki@helsinki.fi>
2022-11-22 21:33:54 +0200

    Invoke tasks for easier commands

---
+++ tasks.py
@@ -0,0 +1,25 @@
+import pty
+from invoke import task
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+    
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)    
+
+
+@task
+def build(ctx):
+    ctx.run("python3 src/initialize_database.py", pty=True)
+
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)

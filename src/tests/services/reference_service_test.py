9159628890e5d926ec43970d22ccf2737896bb54
Sami Männistö <sami.mannisto1@gmail.com>
2022-11-28 12:26:28 +0200

    Add more tests for reference_services

--- src/tests/services/reference_service_test.py
+++ src/tests/services/reference_service_test.py
@@ -22,5 +22,19 @@ class TestReferenceService(unittest.TestCase):
 		result = self.reference_service.references_search("Bergström, Gunilla")
 		self.assertEqual(result[0][1], "Bergström, Gunilla")
 
+	def test_search_returns_correct_value(self):
+		result = self.reference_service.search("Bergström, Gunilla")
+		result = self.assertEqual(result.author, self.book.author)
 
+	def test_search_keyword_not_found(self):
+		result = self.reference_service.search("Halme, Toni")
+		result = self.assertEqual(result, None)
 
+	def test_all_references_are_returned_correctly(self):
+		result = self.reference_service.search_all_ordered_by_descending_datetime()
+		self.assertEqual(result.author, self.book.author)
+	
+	def test_if_no_references_are_found_return_none(self):
+		references_repository.delete_all_book_references()
+		result = self.reference_service.search_all_ordered_by_descending_datetime()
+		self.assertEqual(result, None)

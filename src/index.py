9d3e40d1062c8c73e1c44a02a10c315195823e6f
Janne Kärki <janne.karki@helsinki.fi>
2022-11-30 23:14:17 +0200

    Changed the way how app gets its modules

--- src/index.py
+++ src/index.py
@@ -1,8 +1,11 @@
 from ui.ui import Ui
 from console_io import ConsoleIO
+from repositories.references_repository import ReferencesRepository
+from services.reference_service import ReferenceService
 
 def main():
-    _ui = Ui(ConsoleIO())
+    references_repository = ReferencesRepository()
+    _ui = Ui(ConsoleIO(), ReferenceService(references_repository))
     _ui.start()
 
 if __name__ == "__main__":

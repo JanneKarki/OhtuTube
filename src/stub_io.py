5b50ae0d0b7de5dd9bab6e2b4dbed619f6680212
Janne Kärki <janne.karki@helsinki.fi>
2022-11-30 20:36:18 +0200

    stub_io for robot tests

---
+++ src/stub_io.py
@@ -0,0 +1,16 @@
+
+class StubIO:
+	def __init__(self, inputs=None):
+		self.inputs = inputs or []
+		self.outputs = []
+
+	def read(self, prompt):
+		if len (self.inputs) > 0:
+			return self.inputs.pop(0)
+		return ""
+
+	def write(self, prompt):
+		self.outputs.append(prompt)
+
+	def add_input(self, value):
+		self.inputs.append(value)

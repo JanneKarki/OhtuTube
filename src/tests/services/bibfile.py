e49fb10e741d4622e9c8de4c56de66289548d717
Janne Kärki <janne.karki@helsinki.fi>
2022-12-11 13:57:20 +0200

    Test bibfile

---
+++ src/tests/services/bibfile.py
@@ -0,0 +1,18 @@
+BIBFILE = """@book{mcg2021,
+        author = {"Metz, Cade"},
+        title = {"Genius Makers"},
+        year = {"2021"},
+        publisher = {"Penguin"},
+        address = {"London"}
+        }
+@book{ecb18,
+        author = {"Evans, Claire"},
+        title = {"Broad band"},
+        year = {"2018"},
+        publisher = {"Portfolio/Penguin"},
+        address = {"New York"}
+        }
+		"""
+
+def bibfile():
+        return BIBFILE
